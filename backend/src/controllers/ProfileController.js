3c86fc78c49c99bdae07adcd62aefd8f5ae50f1e
marceloboy1 <marceloboy1@gmail.com>
2020-03-26 23:43:38 -0300

    front end finalizado (rever botao de logout)

--- backend/src/controllers/ProfileController.js
+++ backend/src/controllers/ProfileController.js
@@ -7,7 +7,7 @@ module.exports = {
         const ong_id = request.headers.authorization;
         
         const incidents = await connection('incidents')
-            .where('id', id)
+            .where('ong_id', ong_id)
             .select('*')
         return response.json(incidents);
     }
